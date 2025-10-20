4c09abb5b4a364369a3a1a0e7080451b48949f7e
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2025-10-19 23:57:49 -0400

    Switching to dwm

---
+++ takeactionforthecriticalbatterylevel.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# File where battery information is stored
+BATTERY_STATUS_FILE="/sys/class/power_supply/BAT0/capacity"
+
+# Check if the battery status file exists
+if [ ! -f "$BATTERY_STATUS_FILE" ]; then
+    echo "Battery status file not found!"
+    exit 1
+fi
+
+
+if [ $(cat "$BATTERY_STATUS_FILE") -le 30 ]; then
+    echo "Battery level is critical. Shutting down."
+    systemctl poweroff
+fi
